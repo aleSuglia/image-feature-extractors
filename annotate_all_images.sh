5873c9516d5fb06b1f39c0271682676c0944c867
Alessandro Suglia <alessandro.suglia@gmail.com>
2019-10-21 15:54:12 +0100

    enabled loading images from file path.

--- annotate_all_images.sh
+++ annotate_all_images.sh
@@ -4,24 +4,28 @@ python scripts/extract_features_vg.py \
     -prototxt models/vg_faster_rcnn_end2end/test_rpn.prototxt \
     -caffemodel models/vg_faster_rcnn_end2end/resnet101_faster_rcnn_final.caffemodel \
     -images /datasets/nocaps/images/nd_valid \
+    --from_image_path \
     -output /outputs/nd_valid;
 
 python scripts/extract_features_vg.py \
     -prototxt models/vg_faster_rcnn_end2end/test_rpn.prototxt \
     -caffemodel models/vg_faster_rcnn_end2end/resnet101_faster_rcnn_final.caffemodel \
     -images /datasets/nocaps/images/od_valid \
+    --from_image_path \
     -output /outputs/od_valid;
     
 python scripts/extract_features_vg.py \
   -prototxt models/vg_faster_rcnn_end2end/test_rpn.prototxt \
   -caffemodel models/vg_faster_rcnn_end2end/resnet101_faster_rcnn_final.caffemodel \
   -images /datasets/nocaps/images/nd_test \
+  --from_image_path \
   -output /outputs/nd_test;
 
 python scripts/extract_features_vg.py \
     -prototxt models/vg_faster_rcnn_end2end/test_rpn.prototxt \
     -caffemodel models/vg_faster_rcnn_end2end/resnet101_faster_rcnn_final.caffemodel \
     -images /datasets/nocaps/images/od_test \
+    --from_image_path \
     -output /outputs/od_test;
 
 
